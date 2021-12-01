22733ae2cd39a11a30c81c8e0a1434a87b68cf7e
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2021-12-01 06:21:34 -0600

    Adds day 1 part 2

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,12 +1,24 @@
 defmodule Day1 do
-  def solve(path \\ "lib/day1/puzzleInput.txt") do
+  def part_1(path \\ "lib/day1/puzzleInput.txt") do
     path
-    |> FileReader.get_lines()
-    |> Stream.map(fn value ->
-      value
-      |> Integer.parse(10)
-      |> elem(0)
+    |> get_parsed_values()
+    |> get_increased_tally()
+  end
+
+  def part_2(path \\ "lib/day1/puzzleInput.txt") do
+    path
+    |> get_parsed_values()
+    |> Enum.to_list()
+    |> get_windows([])
+    |> Enum.reverse()
+    |> Stream.map(fn {a, b, c} ->
+      a + b + c
     end)
+    |> get_increased_tally()
+  end
+
+  def get_increased_tally(values) do
+    values
     |> Enum.reduce({0, nil}, fn
       value, {0, nil} ->
         {0, value}
@@ -21,5 +33,27 @@ defmodule Day1 do
         end
     end)
     |> elem(0)
+  end
+
+  def get_windows([a | [b | [c | tail]]], windows) do
+    windows = [{a, b, c} | windows]
+
+    rest = [b | [c | tail]]
+
+    get_windows(rest, windows)
+  end
+
+  def get_windows(_, windows) do
+    windows
+  end
+
+  def get_parsed_values(path) do
+    path
+    |> FileReader.get_lines()
+    |> Stream.map(fn value ->
+      value
+      |> Integer.parse(10)
+      |> elem(0)
+    end)
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,6 +2,10 @@ defmodule Day1Test do
   use ExUnit.Case
 
   test "part 1 sample input" do
-    assert Day1.solve("lib/day1/sampleInput.txt") == 7
+    assert Day1.part_1("lib/day1/sampleInput.txt") == 7
+  end
+
+  test "part 2 sample input" do
+    assert Day1.part_2("lib/day1/sampleInput.txt") == 5
   end
 end
